b93e5fde9954504e4c331b43d67e9df5ceb6772f
yoni_iut_nice <yoni.baroukh@hotmail.com>
2021-01-31 22:50:39 +0100

    rapport + finalisation script

--- Analyse_de_donnees/script/Analyse.R
+++ Analyse_de_donnees/script/Analyse.R
@@ -112,6 +112,7 @@ pie(slices, labels = lbls, col=rainbow(length(lbls)),
 clients <- subset(clients, select = -age_cut)
 # ----------------------------------------------------------------------- #
 
+# ----------------------------------------------------------------------- #
 #distribution de la variable age
 nbRowEA <- round(nrow(clients)*(2/3))
 clients_age_EA <- clients[1:nbRowEA,]
@@ -150,22 +151,22 @@ plot(df$taux_group,
      ylab = " Nombre de clients",
      col = "red",
      main = "Nombre de clients possèdant une deuxième voiture selon leur taux d'endettement")
-
+# ----------------------------------------------------------------------- #
 
 #---------------------------------------------#
 # 2 Identificaton des catégories de véhicules #
 #---------------------------------------------#
-#Critères établit en focntion des résultats trouvés sur largus.fr, pour chaque modèle de véhicule du fichier Catalogue.csv
+#Critères établit en fonction des résultats trouvés sur largus.fr, pour chaque modèle de véhicule du fichier Catalogue.csv
 #Une différenciation pour le modèle Audi A2 1.4 qui est qualifié de monospace (malgré sa courte longueur) sur largus.fr et de citadine par notre modèle
 #https://www.largus.fr/fiche-technique/Audi/A2/I/2002/Monospace+5+Portes/14+75+Reference-708362.html
 
 #Le prix n'est pas entré dans les critères car en fonction des marques haut de gamme (exemple Mercedes) 
 #le prix moyen d'une compacte toutes marques confondues peut être équivalent à un prix d'une citadine chez Mercedes
 
-#les citadines : puissance inférieur a 90 CV || longueur : courte 
-#les compactes : puissance superieur a 90 CV || longueur : courte ou moyenne 
-#les sportives : puissance supérieur a 300 CV
-#les familiale : nb de place > 5, || longueur : longue, très longue
+#les citadines : puissance inférieure à 90 CV || longueur : courte 
+#les compactes : puissance superieure à 90 CV || longueur : courte ou moyenne 
+#les sportives : puissance supérieure à 300 CV
+#les familiales : nb de place > 5, || longueur : longue, très longue
 #les berlines : puissance : entre 100 et 300 CV || longueur : longue, très longue
 
 catalogue$categorie = ifelse(catalogue$puissance<90 & catalogue$longueur == "courte", "citadine",
@@ -192,7 +193,8 @@ immatriculations$categorie = ifelse(immatriculations$puissance<90 & immatriculat
                                     ifelse(immatriculations$puissance>300, "sportive",
                                            ifelse(immatriculations$longueur == "moyenne" | immatriculations$longueur == "courte" & immatriculations$puissance>=90  , "compacte",
                                                   ifelse(immatriculations$nbPlaces>5 & (immatriculations$longueur == "longue" | immatriculations$longueur == "très longue"), "familiale",
-                                                         ifelse(immatriculations$puissance>=100 & immatriculations$puissance<=300 & (immatriculations$longueur == "longue" | immatriculations$longueur == "très longue"), "berline", "autre"
+                                                         ifelse(immatriculations$puissance>=100 & immatriculations$puissance<=300 & 
+                                                                  (immatriculations$longueur == "longue" | immatriculations$longueur == "très longue"), "berline", "autre"
                                                          )
                                                   )
                                            )
@@ -212,6 +214,14 @@ str(clients_categorie$categorie)
 #O ligne selectionné car pas de voiture avec nbPlaces > 5 dans immatriculations 
 count(filter(clients_categorie, categorie == "familiale"))
 
+
+# ----------------------------------------------------------------------- #
+#Les personnes possédant une deuxième voiture opte principalement pour une sportive ou une citadine.
+clients_categorie%>% 
+  group_by(categorie, X2eme.voiture)%>% 
+  summarise(nb = n()) %>% 
+  arrange(desc(nb))
+# ----------------------------------------------------------------------- #
 
 #-----------------------------------------------------------------------------------------------------#
 # 5 Création d'un modèle de classification supervisée pour la prédiction de la catégorie de véhicules #
@@ -395,6 +405,8 @@ test_nb <- function(arg1, arg2){
 test_nb(20, FALSE)
 test_nb(0, TRUE)
 test_nb(20, TRUE)
+test_nb(0, FALSE)
+
 
 
 #---------------------#
